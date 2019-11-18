6da10f8a21108dfa2e807bde1465020d17aefeda
Mogrant <andrewen.ens@ya.ru>
2019-11-18 21:05:45 +0800

    Added Learning sample

--- SA3/Form1.cs
+++ SA3/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,43 @@ namespace SA3
 
         bool debug = true;
 
+        struct LearnEx
+        {
+            ArrayList Arr;
+            ArrayList YCorrect;
+
+            public LearnEx(ArrayList arr, ArrayList ycorrect)
+            {
+                this.Arr = arr;
+                this.YCorrect = ycorrect;
+            }
+
+            public ArrayList LearnArray
+            {
+                get
+                {
+                    return this.Arr;
+                }
+                set
+                {
+                    this.Arr = value;
+                }
+            }
+
+            public ArrayList LearnYCorrect
+            {
+                get
+                {
+                    return this.YCorrect;
+                }
+                set
+                {
+                    this.YCorrect = value;
+                }
+            }
+        }
+
+        List<LearnEx> learns;
 
 
         Color defaultColor = Color.White;
@@ -190,6 +228,61 @@ namespace SA3
                 WriteLineConsole("Created neurons X = " + Xneurons.Count + " Z = " + Zneurons.Count + " Y = " + Yneurons.Count);
             }
         }
+
+        private LearnEx AddLearnEx(string file, int resultY)
+        {
+            if (!System.IO.File.Exists("./" + file))
+            {
+                WriteLineConsole(Color.Red, "File : \"" + file + "\" is not exists!");
+            }
+            System.Drawing.Image image;
+            System.Drawing.Bitmap picture;
+            int[,] picMatrix;
+            try
+            {
+                image = Image.FromFile("./" + file);
+                picture = new Bitmap(image);
+                WriteLineConsole("bitmap created");
+                picMatrix = new int[picture.Width, picture.Height];
+                Color clr;
+                for (int i = 0; i < picture.Width; i++)
+                {
+                    for (int j = 0; j < picture.Height; j++)
+                    {
+                        clr = picture.GetPixel(i, j);
+                        if (clr.R > 0 && clr.G > 0 && clr.B > 0)
+                        {
+                            picMatrix[i, j] = 0;
+                        }
+                        else
+                        {
+                            picMatrix[i, j] = 1;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                picMatrix = new int[0, 0];
+                WriteLineConsole("Error load image");
+            }
+
+            ArrayList arr = new ArrayList();
+            for (int i = 0; i < picMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < picMatrix.GetLength(1); j++)
+                {
+                    WriteConsole(" " + picMatrix[i, j]);
+                    arr.Add(picMatrix[i, j]);
+
+                }
+                WriteLineConsole("");
+            }
+            ArrayList Ycorrect = new ArrayList();
+            Ycorrect.Add(resultY);
+            return new LearnEx(arr, Ycorrect);
+        }
+
         //EVENTS
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -202,7 +295,10 @@ namespace SA3
         {
             AddNeurons();
             Render();
-
+            learns = new List<LearnEx>();
+            learns.Add(AddLearnEx("l1.png", 0));
+            learns.Add(AddLearnEx("l2.png", 0));
+            learns.Add(AddLearnEx("l3.png", 0));
         }
     }
 
